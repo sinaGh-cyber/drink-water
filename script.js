7016fb761aee5db4c9122a41c22fe2ea1cc3d482
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-11-04 04:47:26 +0330

    js ready

--- script.js
+++ script.js
@@ -3,21 +3,47 @@ const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
 
-const fillCup = (idx) => {
-  smallCups.forEach((cup, idx2) => {
-    if (
-      smallCups[idx].classList.contains('full') &&
-      !smallCups[idx].nextElementSibling.classList.contains('full')
-    ) {
-      idx--;
-    }
+const updateBigCup = () => {
+  //   console.log(smallCups.length);
+  const fullSmallCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
 
+  if (fullSmallCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = '0';
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${(fullSmallCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullSmallCups / totalCups) * 100}%`;
+  }
+
+  if (fullSmallCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (fullSmallCups * 250) / 1000}L`;
+  }
+};
+
+updateBigCup();
+
+const fillCup = (idx) => {
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--;
+  }
+  smallCups.forEach((cup, idx2) => {
     if (idx2 <= idx) {
       cup.classList.add('full');
     } else {
       cup.classList.remove('full');
     }
   });
+
+  updateBigCup();
 };
 
 smallCups.forEach((cup, idx) => {
